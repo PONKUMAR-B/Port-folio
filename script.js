131b79ab305616590ec42f4a5b7fab6d1a9b3636
Ponkumar <ponkumarbalasubramanian@gmail.com>
2022-07-28 20:44:26 +0530

    portfolio

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 
+
 var menu=document.querySelector('.menu');
 var navbar=document.querySelector('.nav_bar ul');
 var body=document.querySelector('.head')
 
 menu.addEventListener('click',()=>{
     navbar.classList.toggle('slide');
+    menu.children[0].classList.toggle('line1');
+    menu.children[1].classList.toggle('line2')
+    menu.children[2].classList.toggle('line3')
 });
 
