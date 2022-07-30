9edebea046b27a47af204b42aa6067573369492e
Ponkumar <ponkumarbalasubramanian@gmail.com>
2022-07-30 12:28:26 +0530

    final portfolio

--- script.js
+++ script.js
@@ -2,12 +2,50 @@
 
 var menu=document.querySelector('.menu');
 var navbar=document.querySelector('.nav_bar ul');
-var body=document.querySelector('.head')
+var back=document.querySelector('.nav_bar ul .li1');
+var back1=document.querySelector('.nav_bar ul .li2');
+var back2=document.querySelector('.nav_bar ul .li3');
+var back3=document.querySelector('.nav_bar ul .li4');
+var back4=document.querySelector('.nav_bar ul .li5');
 
 menu.addEventListener('click',()=>{
     navbar.classList.toggle('slide');
     menu.children[0].classList.toggle('line1');
-    menu.children[1].classList.toggle('line2')
-    menu.children[2].classList.toggle('line3')
+    menu.children[1].classList.toggle('line2');
+    menu.children[2].classList.toggle('line3');
 });
 
+back.addEventListener('click',()=>{
+    navbar.classList.remove('slide');
+    menu.children[0].classList.toggle('line1');
+    menu.children[1].classList.toggle('line2');
+    menu.children[2].classList.toggle('line3');
+});
+
+back1.addEventListener('click',()=>{
+    navbar.classList.remove('slide');
+    menu.children[0].classList.toggle('line1');
+    menu.children[1].classList.toggle('line2');
+    menu.children[2].classList.toggle('line3');
+});
+
+back2.addEventListener('click',()=>{
+    navbar.classList.remove('slide');
+    menu.children[0].classList.toggle('line1');
+    menu.children[1].classList.toggle('line2');
+    menu.children[2].classList.toggle('line3');
+});
+
+back3.addEventListener('click',()=>{
+    navbar.classList.remove('slide');
+    menu.children[0].classList.toggle('line1');
+    menu.children[1].classList.toggle('line2');
+    menu.children[2].classList.toggle('line3');
+});
+
+back4.addEventListener('click',()=>{
+    navbar.classList.remove('slide');
+    menu.children[0].classList.toggle('line1');
+    menu.children[1].classList.toggle('line2');
+    menu.children[2].classList.toggle('line3');
+});
